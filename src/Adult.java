155fe3129d461a91052ad0347696be864d29be0e
rmit-s3677348-sumit-sitlani <rmit-s3677348-sumit-sitlani@users.noreply.github.com>
2018-03-28 03:22:12 +1100

    Adult and Dependent Classes

---
+++ src/Adult.java
@@ -0,0 +1,50 @@
+//@author Sumit Sitlani
+  
+package minNetPackage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Adult extends Person 
+{
+	private List<Adult>listOfPeople = new ArrayList<Adult>();
+	private boolean hasPartner = false;
+	private boolean hasChild = false;
+	
+	public Adult(String id, String name, int age)
+	{
+		super(id, name, age);
+	}
+	
+	public Adult()
+	{
+		super();
+	}
+	
+	//private List<Connection> c1 = new ArrayList<Connection>();
+	
+	public List<Adult> getListOfPeople()
+	{
+		return this.listOfPeople;
+	}
+	
+	public void setPartner(boolean partner)
+	{
+		this.hasPartner = partner;
+	}
+
+	public boolean getHasPartner()
+	{
+		return this.hasPartner;
+	}
+	
+	public void setHasChild(boolean child)
+	{
+		this.hasChild = child;
+	}
+	
+	public boolean getHasChild()
+	{
+		return this.hasChild;
+	}
+}
---
+++ src/Dependent.java
@@ -0,0 +1,30 @@
+//@author Sumit Sitlani
+
+
+package minNetPackage;
+
+
+public class Dependent extends Person
+{
+	private boolean hasParent = false;
+	public Dependent(String id, String name, int age)
+	{
+		super(id, name, age);
+	}
+	
+	public Dependent()
+	{
+		
+	}
+	
+	public void setHasParent(boolean parent)
+	{
+		this.hasParent = parent;
+	}
+	
+	public boolean getHasParent()
+	{
+		return this.hasParent;
+	}
+	//private List<Connection> c1; 
+}
